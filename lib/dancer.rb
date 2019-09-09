17e592cb263904f503b5fd20193c2c9d8d9da86e
Olivia Kesler <oakesler@protonmail.ch>
2019-09-09 17:41:48 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,47 @@
+require_relative './fancy_dance.rb'
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+  extend MetaDancing
+  include Dance 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end
+end
+
+
+
+
+
+
+
+
+#class Dancer
+  #extend FancyDance::ClassMethods
+  #include FancyDance::InstanceMethods
+  
+  #attr_accessor :name 
+  
+  #def initialize(name)
+    #@name = name
+  #end
+#end
+
+
+
+
+
+#class Dancer
+  #extend MetaDancing
+  #include Dance 
+  
+  #attr_accessor :name 
+  
+  #def initialize(name)
+    #@name = name
+  #end
+#end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,56 @@
+require_relative './fancy_dance.rb'
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+
+
+class Kid 
+  extend MetaDancing
+  include Dance 
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#class Kid
+  #extend FancyDance::ClassMethods
+  #include FancyDance::InstanceMethods
+  
+  #attr_accessor :name 
+  
+  #def initialize(name)
+    #@name = name
+  #end
+#end
+
+
+
+
+
+
+#class Kid 
+  #extend MetaDancing
+  #include Dance 
+  
+  #attr_accessor :name 
+  
+  #def initialize(name)
+    #@name = name
+  #end
+#end
