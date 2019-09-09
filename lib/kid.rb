d27bb41d3cafac2d07d886f279771d6d61281f8d
Olivia Kesler <oakesler@protonmail.ch>
2019-09-09 17:28:09 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,13 +1,15 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid 
-  extend MetaDancing
-  include Dance 
+
+
+#class Kid 
+  #extend MetaDancing
+  #include Dance 
   
-  attr_accessor :name 
+  #attr_accessor :name 
   
-  def initialize(name)
-    @name = name
-  end
-end
+  #def initialize(name)
+    #@name = name
+  #end
+#end
