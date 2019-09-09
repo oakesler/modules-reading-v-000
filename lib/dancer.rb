c80ab030e9b0260611704ade6db35a48636043aa
Olivia Kesler <oakesler@protonmail.ch>
2019-09-09 17:27:35 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,13 +1,15 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer
-  extend MetaDancing
-  include Dance 
+
+
+#class Dancer
+  #extend MetaDancing
+  #include Dance 
   
-  attr_accessor :name 
+  #attr_accessor :name 
   
-  def initialize(name)
-    @name = name
-  end
-end
+  #def initialize(name)
+    #@name = name
+  #end
+#end
