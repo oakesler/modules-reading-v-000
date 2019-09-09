f4da03be9ca57926b34338968b2f63d8a7281b29
Olivia Kesler <oakesler@protonmail.ch>
2019-09-09 17:31:22 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -5,6 +5,12 @@ require_relative './dance_module.rb'
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+  
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name
+  end
 end
 
 #class Dancer
